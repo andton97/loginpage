9c67e4c25be348b55ea8fc9955da910879b025d0
andton97 <45421042+andton97@users.noreply.github.com>
2023-10-02 18:40:19 +0200

    Create Login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "container">
+      <div className = "login">
+          <div className='login__form'>
+            <form class="form">
+            <div className='form__mail'>
+              <input className='form__input' type='text' placeholder='username'/>
+            </div>
+            <div className='form__password'>
+              <input class="form__input" type="password" placeholder='password'/>
+            </div>
+            <div className='form__element'>
+              <button className='form__btn' type='submit'>
+                <span className='form__btn--text'>Login</span>
+              </button>
+            </div>
+            <div className='form__element'>
+              <p className='form__message'>Not registered? Create an account</p>
+            </div>
+
+          </form>
+            
+          </div>
+      </div>
     </div>
   );
 }
